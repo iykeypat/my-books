63404ea1a8989256e6707840d43e8f6a94ea050a
Iykeypat <patrickiykey@gmail.com>
2022-05-09 11:12:32 +0100

    I added a new table: "Publisher". it shares a one to many relationship with the book table.

--- Data/Models/Book.cs
+++ Data/Models/Book.cs
@@ -17,5 +17,9 @@ namespace my_books.Data.Models
         public int? Rate { get; set; }
         public string CoverUrl { get; set; }
         public DateTime DateAdded { get; set; }
+
+        //Navigation properties
+        public int? PublisherId { get; set; }
+        public Publisher Publisher { get; set; }
     }
 }
