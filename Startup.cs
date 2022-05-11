3406a3bbb4cfebba63fcc824ef25ed0a6718ac9f
Iykeypat <patrickiykey@gmail.com>
2022-05-11 16:14:39 +0100

    api versioning

--- Startup.cs
+++ Startup.cs
@@ -43,6 +43,8 @@ namespace my_books
             services.AddTransient<PublishersService>();
             services.AddTransient<AuthorsService>();
 
+            //services.AddApiVersioning();
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "my_books", Version = "v1" });
